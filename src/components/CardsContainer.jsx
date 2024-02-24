caad43c78fa0e20946827ec3677aa98b67d79f2d
Muzammil <muzammilsiddiqui826@gmail.com>
2024-02-24 12:59:31 +0530

    Fix saved projects

--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -8,16 +8,16 @@ import Loader from './Loader';
 const CardsContainer = ({ projects }) => {
 
     return (
-        projects ?
-            <div className='w-[80vw] mx-auto'>
-                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                    {projects.map((project, index) => (
-                        <Grid item xs={4} sm={4} md={4} key={index}>
-                            <ProjectCard project={project} />
-                        </Grid>
-                    ))}
-                </Grid>
-            </div> : <div className='text-center'>No Projects Found</div>
+        projects.length > 0 ? 
+        <div className='w-[80vw] mx-auto'>
+            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                {projects.map((project, index) => (
+                    <Grid item xs={4} sm={4} md={4} key={index}>
+                        <ProjectCard project={project} />
+                    </Grid>
+                ))}
+            </Grid>
+        </div> : <div className='text-center'>No Projects Found</div>
     )
 }
 
